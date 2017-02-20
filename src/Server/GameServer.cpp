2e9878f9e0a6a50b450bc88edef2d32f8841894d
Olha <olhalesk@gmail.com>
2017-02-20 15:31:43 +0200

    Bug is fixed. Correct answer when a player loads terrains list (by type)

--- src/Server/GameServer.cpp
+++ src/Server/GameServer.cpp
@@ -427,7 +427,7 @@ void fSendDefinedTerrains(std::string &json_response, nlohmann::json &json_reque
     {
         string type = json_request["type"];
         string count = json_request["count"];
-        string query = "SELECT id, name, width, height, description, id_owner FROM Terrain LIMIT "+count+";";
+        string query = "SELECT id, name, type, width, height, description, id_owner FROM Terrain WHERE type = '" + type + "' LIMIT " + count + ";";
         nlohmann::json json_result = data_base.fExecuteQuery(query);
         cout << query << "\nRESULT:\n" << json_result << endl;
         string query_result = json_result["result"];
